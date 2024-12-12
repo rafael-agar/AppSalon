862a54a81401282395a522684bcd5ac9bb482d19
Rafael Agar <i.t.rafaelagar@gmail.com>
2024-12-12 11:01:04 -0400

    build in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,7 @@ function procesarImagenes(file, outputSubDir) {
     if (extName.toLowerCase() === '.svg') {
         // If it's an SVG file, move it to the output directory
         const outputFile = path.join(outputSubDir, `${baseName}${extName}`);
-    fs.copyFileSync(file, outputFile);
+        fs.copyFileSync(file, outputFile);
     } else {
         // For other image formats, process them with sharp
         const outputFile = path.join(outputSubDir, `${baseName}${extName}`);
@@ -73,4 +73,6 @@ export function dev() {
     watch('src/img/**/*.{png,jpg}', imagenes)
 }
 
-export default series( js, css, imagenes, dev )
+export const build = series(js, css, imagenes);
+
+export default series(build, dev);
